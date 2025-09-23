348da8e5da0f4a507ca4439207a4173af33c83d2
Gina Deanna <ginadeanna@gmail.com>
2025-09-23 16:23:11 -0300

    traigo rama del repo

--- src/main/java/com/tallerwebi/dominio/ServicioSalaImpl.java
+++ src/main/java/com/tallerwebi/dominio/ServicioSalaImpl.java
@@ -16,43 +16,43 @@ public class ServicioSalaImpl implements ServicioSala{
     public ArrayList<Sala> getSalas() {
         ArrayList<Sala> salas = new ArrayList<>();
 
-//        Sala sala1 = new Sala(
-//                1,
-//                "La Mansión Misteriosa",
-//                "Principiante",
-//                "Mansión",
-//                "Una noche tormentosa te encuentras atrapado en una vieja mansión llena de acertijos.",
-//                true,
-//                null,
-//                null
-//        );
-//
-//        Sala sala2 = new Sala(
-//                2,
-//                "El Laboratorio Secreto",
-//                "Intermedio",
-//                "Laboratorio",
-//                "Un científico desaparecido dejó pistas en su laboratorio. ¿Podrás descubrir qué tramaba?",
-//                true,
-//                null,
-//                null
-//        );
-//
-//
-//                Sala sala3 = new Sala(
-//                3,
-//                "La Cárcel Abandonada",
-//                "Avanzado",
-//                "Prisión",
-//                "Despiertas en una celda oxidada. Solo resolviendo complejos acertijos podrás escapar.",
-//                false,
-//                        null,
-//                        null
-//        );
-//
-//        salas.add(sala1);
-//        salas.add(sala2);
-//        salas.add(sala3);
+        Sala sala1 = new Sala(
+                1,
+                "La Mansión Misteriosa",
+                "Principiante",
+                "Mansión",
+                "Una noche tormentosa te encuentras atrapado en una vieja mansión llena de acertijos.",
+                true,
+                null,
+                null
+        );
+
+        Sala sala2 = new Sala(
+                2,
+                "El Laboratorio Secreto",
+                "Intermedio",
+                "Laboratorio",
+                "Un científico desaparecido dejó pistas en su laboratorio. ¿Podrás descubrir qué tramaba?",
+                true,
+                null,
+                null
+        );
+
+
+                Sala sala3 = new Sala(
+                3,
+                "La Cárcel Abandonada",
+                "Avanzado",
+                "Prisión",
+                "Despiertas en una celda oxidada. Solo resolviendo complejos acertijos podrás escapar.",
+                false,
+                        null,
+                        null
+        );
+
+        salas.add(sala1);
+        salas.add(sala2);
+        salas.add(sala3);
 
         return salas;
     }
