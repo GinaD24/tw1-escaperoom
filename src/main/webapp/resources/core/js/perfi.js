0ab9cd8d53d694dff59d9acc30d8a5b508bbeaa1
Alan Thomas <alanmt555@gmail.com>
2025-10-27 00:46:28 -0300

    arreglo de css, implementacion de mercado pago

--- src/main/webapp/resources/core/js/perfi.js
+++ src/main/webapp/resources/core/js/perfi.js
@@ -1,1 +1,31 @@
 
+    var fileUpload = document.getElementById("fileUpload");
+   var previewContainer = document.getElementById("previewContainer");
+   var inputFile = document.getElementById("imagen");
+   var preview = document.getElementById("preview");
+   var uploadContent = document.getElementById("uploadContent");
+   var botonCambiarFoto = document.getElementById("botonCambiarFoto");
+
+   previewContainer.style.display = "block";
+
+   if (!preview.src || preview.src === "") {
+       preview.src = ""; // o una imagen de placeholder si querés
+       preview.style.backgroundColor = "#ffff00";
+       preview.style.objectFit = "contain";
+       preview.style.opacity = "0.3";
+   }
+
+   inputFile.addEventListener('change', (event) => {
+       var file = event.target.files[0];
+       if (file) {
+           const url = URL.createObjectURL(file);
+           preview.src = url;
+           uploadContent.style.display = "none";
+           previewContainer.style.display = "block";
+           preview.style.opacity = "1"; // volver a opaco al tener imagen
+       }
+   });
+
+   botonCambiarFoto.addEventListener('click', () => {
+       inputFile.click();
+   });
